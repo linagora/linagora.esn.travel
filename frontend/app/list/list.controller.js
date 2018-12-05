19c3bb158fb810bbf5baea44b158bae4151930c8
Christophe HAMERLING <chamerling@linagora.com>
2018-12-05 16:21:26 +0100

    Remove infinitescroll from travels

--- frontend/app/list/list.controller.js
+++ frontend/app/list/list.controller.js
@@ -5,30 +5,19 @@
     .controller('TravelListController', TravelListController);
 
   function TravelListController(
-    _,
     $modal,
-    infiniteScrollHelper,
     travelApiClient
   ) {
     var self = this;
-    var DEFAULT_LIMIT = 20;
-
-    self.options = {
-      offset: 0,
-      limit: DEFAULT_LIMIT
-    };
 
     self.$onInit = $onInit;
 
     function $onInit() {
       self.onCreateBtnClick = onCreateBtnClick;
-      self.loadMoreElements = infiniteScrollHelper(self, _loadNextItems);
-    }
 
-    function _loadNextItems() {
-      self.options.offset = self.elements.length;
-
-      return travelApiClient.list(self.options);
+      travelApiClient.list().then(function(travels) {
+        self.travels = travels;
+      });
     }
 
     function onCreateBtnClick() {
