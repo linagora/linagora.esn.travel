b34b40d8dc0aa2838b848dc9a86867d444fe39d3
Christophe HAMERLING <chamerling@linagora.com>
2018-12-07 15:18:53 +0100

    Mock transport data

--- frontend/app/create/create.controller.js
+++ frontend/app/create/create.controller.js
@@ -1,12 +1,23 @@
 (function(angular) {
   'use strict';
 
+  var plane = { air: { 'outbound-airport': 'NCE', 'outbound-flight': { company: 'EZ', 'take-off': '2018-11-19', 'flight-reference': 'EZ3454', 'current-price': 34.56 }, 'return-airport': 'CDG', 'return-flight': { company: 'EZ', 'take-off': '2018-11-22', 'flight-reference': 'EZ3455', 'current-price': 143.65 } } };
+  var car = { road: { 'from-adress': 'Grasse city', 'to-adress': 'Nice airport', distance: 33 } };
+  var train = { railway: { 'outbound-station': 'Cannes', 'outbound-train': { company: 'SNCF', departure: '2018-11-19', 'train-reference': '3454', 'current-price': 34.56 }, 'return-station': 'Marseille', 'return-train': { company: 'OuiGo', 'take-off': '2018-11-22', 'train-reference': '3455', 'current-price': 143.65 } } };
+
+  var mockTransportData = {
+    Plane: plane,
+    Car: car,
+    Train: train
+  };
+
   angular.module('linagora.esn.travel')
     .controller('TravelCreateController', TravelCreateController);
 
   function TravelCreateController(travelApiClient) {
     var self = this;
 
+    self.travelModes = ['Car', 'Plane', 'Train'];
     self.create = create;
     self.onStartDateChange = onStartDateChange;
     self.onEndDateChange = onEndDateChange;
@@ -16,6 +27,8 @@
     });
 
     function create() {
+      self.travel.transports = mockTransportData[self.travelMode];
+
       travelApiClient.create(self.travel).then(function() {
         console.log('Created');
       });
